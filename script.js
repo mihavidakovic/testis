ce58fd189e8ae76c054b1194543f3c87372e9233
Josip Rajkovic <Josip Rajkovic>
2017-02-25 23:16:57 +0100

    zbrisan script iz roota

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log('dasd');
-const slika = document.getElementById('#testna');
-
-function resize() {
-	console.log('drek');
-	slika.style.width = '50px';
-}
-
-slika.addEventListener('click', resize);
