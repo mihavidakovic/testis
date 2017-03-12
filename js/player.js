2269bf4dfda4a09f0800108bae17f6222f092597
Josip Rajkovic <Josip Rajkovic>
2017-03-12 23:55:27 +0100

    testno state visible blablabalba

--- js/player.js
+++ js/player.js
@@ -2,20 +2,39 @@
 const player = document.querySelector('.video');
 const video = player.querySelector('.viewer');
 const playPause = player.querySelector('.video-play');
-const tipkaPP = player.querySelector('.ion-ios-play');
+const tipkaPP = playPause.querySelector('.ion-ios-play');
 const fullScreen = player.querySelector('.ion-android-expand');
 const progressBar = player.querySelector('.progress-current');
+const statusCircle =  player.querySelector('.state');
+
+//status krog na sredini
+var showStatusCircle = false;
+
+function statusCircleUpdate() {
+	if (showStatusCircle == true) {
+		showStatusCircle = false;
+		statusCircle.classList.remove('visible');
+	}
+}
+
 
 //funkcije playerja
 function playPauseVideo() {
 	if (video.paused == true) {
 		tipkaPP.className = "ion-ios-pause";
+		showStatusCircle = true;
+		statusCircle.classList.add('visible');
 		video.play();
 	} else {
 		tipkaPP.className = "ion-ios-play";
+		showStatusCircle = true;
+		statusCircle.classList.add('visible');
 		video.pause();
 	}
+	setTimeout(statusCircleUpdate, 400);
 }
+
+
 
 function fullScreenExpand() {
 	if (video.requestFullscreen) {
@@ -36,5 +55,6 @@ function handleProgressBar() {
 //event listenerji za player
 video.addEventListener('click', playPauseVideo);
 playPause.addEventListener('click', playPauseVideo);
+statusCircle.addEventListener('click', playPauseVideo);
 fullScreen.addEventListener('click', fullScreenExpand);
 video.addEventListener('timeupdate', handleProgressBar);
