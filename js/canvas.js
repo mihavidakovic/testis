fe90468be4879c7b744f242aef5c889af1b222fb
Josip Rajkovic <Josip Rajkovic>
2017-03-26 16:28:29 +0200

    meme generator v1.0

--- js/canvas.js
+++ js/canvas.js
@@ -4,19 +4,30 @@ const predogled = document.getElementById('predogled');
 
 //lastnosti kanvasa
 var ctx=canvas.getContext("2d");
-ctx.font="20px Georgia";
+
 //funkcije kanvasa
-function memePreview() {
+function memePreview() {	
 	var zgornjiTekst = document.getElementById('zgornji').value;
 	var spodnjiTekst = document.getElementById('spodnji').value;
-	if (zgornjiTekst == null || spodnjiTekst == null) {
+	if (zgornjiTekst == null || spodnjiTekst == null ) {
 		console.log(zgornjiTekst, spodnjiTekst);
 	} else {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "white";
+	ctx.strokeStyle = "black";
+	ctx.font="100px impact";	
 	ctx.fillText(zgornjiTekst, 10,50);
-	ctx.fillText(spodnjiTekst, 10, 250);
+	ctx.fillText(spodnjiTekst, 10,150);
+	ctx.lineWidth = 2;
+	ctx.strokeText(zgornjiTekst, 10,50);
+	ctx.strokeText(spodnjiTekst, 10,150);
 }
 }
 
+function dodajUrl() {
+	var urlSlike = document.getElementById('url').value;
+	canvas.style.backgroundImage = "url('"+ urlSlike + "')";
+}
+
 //eventlistenerji
-predogled.addEventListener('click', memePreview)
+predogled.addEventListener('click', memePreview);
