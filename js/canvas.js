a3c5b9c3d3ac4a784628878c4dd13ba2ad71d942
Josip Rajkovic <Josip Rajkovic>
2017-03-28 14:42:15 +0200

    popravek kode v generatorju, čakamo na vitanov nasvet

--- js/canvas.js
+++ js/canvas.js
@@ -1,18 +1,20 @@
 //elementi kanvasa
 var canvas = document.getElementById('meme');
-const predogled = document.getElementById('predogled');
+const shrani = document.getElementById('shrani');
+const dodajSliko = document.getElementById('dodaj');
+const reset = document.getElementById('reset');
 
 //lastnosti kanvasa
-var ctx=canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 
-//funkcije kanvasa
+//FUNKCIJE KANVASA
+//Preview funkcija
 function memePreview() {	
 	var zgornjiTekst = document.getElementById('zgornji').value;
 	var spodnjiTekst = document.getElementById('spodnji').value;
 	if (zgornjiTekst == null || spodnjiTekst == null ) {
 		console.log(zgornjiTekst, spodnjiTekst);
 	} else {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = "white";
 	ctx.strokeStyle = "black";
 	ctx.font="100px impact";	
@@ -21,13 +23,34 @@ function memePreview() {
 	ctx.lineWidth = 2;
 	ctx.strokeText(zgornjiTekst, 10,50);
 	ctx.strokeText(spodnjiTekst, 10,150);
+
 }
 }
 
+//potreben element za kasnejšo manipulacijo
+img = document.createElement('img');
+
+//funkcija, ki doda vnešeni url v kanvas
 function dodajUrl() {
 	var urlSlike = document.getElementById('url').value;
 	canvas.style.backgroundImage = "url('"+ urlSlike + "')";
+	img.src = urlSlike;
+	ctx.drawImage(img, 0, 0, 500, 500);
+}
+
+//funkcija, ki pretvori canvas v url
+function shraniMeme() {
+	var dataURL = canvas.toDataURL;	
+	console.log(dataURL);
+}
+
+function ponastaviMeme() {
+	ctx.clearRect(0, 0, 500, 500);
 }
 
 //eventlistenerji
-predogled.addEventListener('click', memePreview);
+shrani.addEventListener('click', shraniMeme);
+dodajSliko.addEventListener('click', dodajUrl);
+reset.addEventListener('click', ponastaviMeme);
+//
+//"'" + dataURL + "'"
