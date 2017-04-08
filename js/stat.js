a95d381e670ce201abafba5a0cfb4a7d2bfcbdde
Josip Rajkovic <Josip Rajkovic>
2017-04-08 21:27:53 +0200

    končno

--- js/stat.js
+++ js/stat.js
@@ -5,6 +5,31 @@ const slika = document.getElementById('slika');
 const downvote = document.getElementById('downvote');
 const upvote = document.getElementById('upvote');
 
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+};
+
+getJSON('http://slomemes.si/posti',
+function(err, data) {
+  if (err != null) {
+    alert('Something went wrong: ' + err);
+  } else {
+    alert('Your query count: ' + data.query.count);
+  }
+});
+
+
 //JSON load funkcija (nevem kaj se tukaj dogaja, skopirano z neta)
 function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
@@ -38,24 +63,30 @@ slika.src = zbirka[indeks].url;
 
 });
 
+var zbirkaPregledanih = [];
 //FUNKCIJE
-
 //funkcija za prestavljanje
 function naprej() {
 	if(indeks >= zbirka.length - 1 || indeks < 0) { //tukaj omejimo delovanje funkcije na velikost jsona v našem primeru 20
-		indeks = Math.floor(Math.random() * 21); //random 0->20
+		indeks = Math.floor(Math.random() * zbirka.length); //random 0->velikost zbirke
 		naslov.innerHTML = ""; //ponastavimo tekst na prazen string, če ne nam jih sešteva
 		ustvarjeno.innerHTML = "";
 		naslov.innerHTML = naslov.innerHTML + zbirka[indeks].title; //nastavimo tekst na vrednost polja v json zbirki z trenutnim indeksom
-		ustvarjeno.innerHTML = "Ustvarjeno: " + zbirka[indeks].created_at;
+		ustvarjeno.innerHTML = "Ustvarjeno: " + zbirka[indeks].created_at + " " + zbirka[indeks].id;
 		slika.src = zbirka[indeks].url;
+		zbirka.splice(indeks, 1);
+		zbirkaPregledanih.push(indeks);
+		console.log(zbirkaPregledanih.length);
 	} else {
-		indeks = Math.floor(Math.random() * 21); //random 0->20
+		indeks = Math.floor(Math.random() * zbirka.length); //random 0->velikost zbirke
 		naslov.innerHTML = "";
 		ustvarjeno.innerHTML = "";
 		naslov.innerHTML = naslov.innerHTML + zbirka[indeks].title;
-		ustvarjeno.innerHTML = "Ustvarjeno: " + zbirka[indeks].created_at;
+		ustvarjeno.innerHTML = "Ustvarjeno: " + zbirka[indeks].created_at + " " + zbirka[indeks].id;
 		slika.src = zbirka[indeks].url;
+		zbirka.splice(indeks, 1);
+		zbirkaPregledanih.push(indeks);
+		console.log(zbirkaPregledanih.length);
 	}
 
 }
