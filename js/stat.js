6684adb93ba42131628145798c0936aa82831d74
Josip Rajkovic <Josip Rajkovic>
2017-04-08 15:43:21 +0200

    delujoči random objekti json

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,14 @@
+//Elementi
+const naslov = document.getElementById('naslov');
+const ustvarjeno = document.getElementById('ustvarjeno');
+const slika = document.getElementById('slika');
+const downvote = document.getElementById('downvote');
+const upvote = document.getElementById('upvote');
+
 function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
 xobj.overrideMimeType("application/json");
-xobj.open('GET', 'countries.json', true);
+xobj.open('GET', 'posti.json', true);
 xobj.onreadystatechange = function () {
 if (xobj.readyState == 4 && xobj.status == "200") {
 
@@ -13,12 +20,44 @@ callback(xobj.responseText);
 xobj.send(null);
 
 }
+//začetna pozicija v json zbirki, ki je naključna vrednost v našem primeru 0->20
+var indeks = Math.floor(Math.random() * 21);
 
 // Call to function with anonymous callback
 loadJSON(function(response) {
-// Do Something with the response e.g.
-var drek = JSON.parse(response);
-// Assuming json data is wrapped in square brackets as Drew suggests
-console.log("drek ", drek[0].name);
+//Deklaracija json responsa v spremenljivko, ki deluje kot zbirka (globalno dostopna)
+zbirka = JSON.parse(response);
+
+//Začetna pozicija indeksa in prikaz slike (stvar spremembe)
+naslov.innerHTML = naslov.innerHTML + zbirka[indeks].title;
+ustvarjeno.innerHTML = ustvarjeno.innerHTML + zbirka[indeks].created_at;
+slika.src = zbirka[indeks].url;
+
+
 
 });
+
+//Funkcije
+//funkcija za prestavljanje
+function naprej() {
+	if(indeks >= zbirka.length - 1 || indeks < 0) {
+		indeks = Math.floor(Math.random() * 21);
+		naslov.innerHTML = "";
+		ustvarjeno.innerHTML = "";
+		naslov.innerHTML = naslov.innerHTML + zbirka[indeks].title;
+		ustvarjeno.innerHTML = "Ustvarjeno: " + zbirka[indeks].created_at;
+		slika.src = zbirka[indeks].url;
+	} else {
+		indeks = Math.floor(Math.random() * 21);
+		naslov.innerHTML = "";
+		ustvarjeno.innerHTML = "";
+		naslov.innerHTML = naslov.innerHTML + zbirka[indeks].title;
+		ustvarjeno.innerHTML = "Ustvarjeno: " + zbirka[indeks].created_at;
+		slika.src = zbirka[indeks].url;
+	}
+
+}
+
+
+upvote.addEventListener('click', naprej);
+downvote.addEventListener('click', naprej);
